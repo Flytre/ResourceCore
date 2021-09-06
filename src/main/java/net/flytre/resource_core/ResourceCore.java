53e7ebcb69c3c519c1874f6ed305e82aec08e2de
Flytre <40476907+flytre@users.noreply.github.com>
2021-09-06 10:14:49 -0400

    No fabric API
    Signed-off-by: Flytre <flytre7@gmail.com>

--- src/main/java/net/flytre/resource_core/ResourceCore.java
+++ src/main/java/net/flytre/resource_core/ResourceCore.java
@@ -1,14 +1,7 @@
 package net.flytre.resource_core;
 
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
-import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
-import net.minecraft.block.Block;
-import net.minecraft.block.Blocks;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
 
-import java.util.function.Supplier;
 import java.util.logging.Logger;
 
 public class ResourceCore implements ModInitializer {
